216ea16e2a22f1d88a00a55e8591a739169dcca6
Bartek <jonaszallegro@gmail.com>
2021-04-10 09:47:23 +0200

    radio buttons parsing answ

--- Lecture 2/bmi/app/src/main/java/com/example/bmi/BMR_calculator.java
+++ Lecture 2/bmi/app/src/main/java/com/example/bmi/BMR_calculator.java
@@ -12,6 +12,8 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class BMR_calculator extends AppCompatActivity {
@@ -21,7 +23,7 @@ public class BMR_calculator extends AppCompatActivity {
     TextView result;
     String calculation;
     private Button bmrButton;
-
+    RadioGroup radioGender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,15 +31,45 @@ public class BMR_calculator extends AppCompatActivity {
 
         result = findViewById(R.id.result);
 
+        radioGender=(RadioGroup)findViewById(R.id.RadioGender);
 
+        radioGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // TODO Auto-generated method stub
+                int childCount = group.getChildCount();
+                String gender=null;
+                for (int x = 0; x < childCount; x++) {
+                    RadioButton btn = (RadioButton) group.getChildAt(x);
+
+
+                    if(btn.getId()==R.id.male){
+                        btn.setText("M");
+                    }else{
+                        btn.setText("F");
+                    }
+                    if (btn.getId() == checkedId) {
+
+                        gender=btn.getText().toString();
+
+                    }
+
+                }
+
+
+            }
+        });
     }
 
-    public void calculateBMI(View view){
+    public void calculateBMR(View view){
 //        double weight = Float.parseFloat(w.getText().toString());
 //        double height = Float.parseFloat(h.getText().toString())/100;
         double bmi = 23;
         calculation = "BMI = " + String.format("%,.2f", bmi);
         result.setText(calculation);
     }
+
+
 
 }
